3dddacd699806b0a9ada9fb0eb0de97b4ef4540e
Prateek7805 <prateekpjimmy@gmail.com>
2023-07-06 23:16:34 +0530

    Initial commit

---
+++ src/Pages/Welcome/Welcome.js
@@ -0,0 +1,28 @@
+import './Welcome.css'
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import Form from 'react-bootstrap/Form';
+export default function Welcome() {
+    return (
+        <>
+            <div className='op main-container container-fluid'>
+                <div className="blur-overlay"></div>
+                <div className='row content'>
+                    <div className='panel col-12 col-sm' >
+                        <div className='panel-title'><p>Welcome Aboard!</p></div>
+                        <div className='d-flex p-3'>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                            <Form.Control placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                        </InputGroup>
+
+                        </div>
+                        
+                    </div>
+                    <div className='panel panel-image col-12 col-sm-6 d-none d-sm-flex' ></div>
+                </div>
+            </div>
+        </>
+    )
+}
